61735f38a6ddc35d588018bd7af2944ed01341c9
tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>
2025-11-23 20:02:31 +0200

    fix: switch to vitest, update scripts
    Signed-off-by: tunnckoCore <5038030+tunnckoCore@users.noreply.github.com>

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import isCI from "is-ci";
+import { defineConfig } from "vitest/config";
+export default defineConfig({
+  test: {
+    include: ["new-test/**/*.test.ts"],
+    exclude: ["**/node_modules/**"],
+    coverage: {
+      enabled: process.env.COV === "1" || isCI,
+      clean: true,
+      cleanOnRerun: true,
+      reporter: ["text", "lcovonly", "html-spa"],
+    },
+    // NOTE: seems to not work..
+    // typecheck: {
+    //   enabled: true,
+    //   tsconfig: "./tsconfig.json",
+    // },
+  },
+});
